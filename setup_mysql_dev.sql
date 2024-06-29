f99f4063aafe1ab7902895deed7fca844369dfd4
Doctware <yusclever9@gmail.com>
2024-06-30 00:47:24 +0100

    setting up Mysql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,16 @@
+-- Preparing MySQL server
+
+-- Creating my Database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Creating a new use
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Granting user hbnb_dev all privileges on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* ON 'hbnb_dev'@'localhost';
+
+-- Granting 'SELECT' privilege to user hbnb_dev on perfomance_schema
+GRANT SELECT ON performance_schema.* ON 'hbnb_dev'@'localhost';
+
+-- apply changes
+FLUSH PRIVILEGES;
