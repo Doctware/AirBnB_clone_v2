387bb2e9b9149c75d7f09722ab2c2a77e9cac06c
Doctware <yusclever9@gmail.com>
2024-06-30 00:49:50 +0100

    setting up Mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,10 +7,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Granting user hbnb_dev all privileges on hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* ON 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Granting 'SELECT' privilege to user hbnb_dev on perfomance_schema
-GRANT SELECT ON performance_schema.* ON 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- apply changes
 FLUSH PRIVILEGES;
