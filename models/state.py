d0cbcc354faf2258ad273b34d90d2ade509c93ac
Doctware <yusclever9@gmail.com>
2024-07-19 01:56:33 +0100

    ORM

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from model.city import City
+import models import storage
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Colum(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    def __init__(self, *args, **kwargs):
+        """ iheriting from BaseModel """
+        super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """
+        Getter
+        this method returns the list of City instances with
+        state_id equls to the current State.id => it will be the file storage
+        relationship between state and city
+        """
+
+        city_list = []
+        for city for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
