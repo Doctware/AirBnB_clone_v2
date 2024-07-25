5b8ab39154a6c01a79288c7c1dddd6b2921cf23b
Doctware <yusclever9@gmail.com>
2024-07-25 13:36:11 +0100

    the console

--- console.py
+++ console.py
@@ -116,7 +116,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage._FileStorage__objects[key])
+            print(storage.__FileStorage__objects[key])
         except KeyError:
             print("** no instance found **")
 
