b4d21b87ffe40e609ef90435880a743e71d377ba
Doctware <yusclever9@gmail.com>
2024-08-12 11:55:39 +0100

    Let route python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" this module statr a web Application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ this method say!!! HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def return_hbnb():
+    """ this module say!! hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def about_c(text):
+    """ this module says something about C """
+    formated_text = text.replace('_', ' ')
+    return f"C {formated_text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>/', strict_slashes=False)
+def route_python(text):
+    """ this method say something about python """
+    txt_fmrt = text.replace('_', ' ')
+    return f"Python {txt_fmrt}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
